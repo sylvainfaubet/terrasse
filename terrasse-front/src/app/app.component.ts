70b37839d642e42a85429e2c92287c29c88b04da
sylvain faubet <faubet.sylvain@gmail.com>
2018-08-12 18:57:35 +0200

    setup footer

--- terrasse-front/src/app/app.component.ts
+++ terrasse-front/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-	//title = 'Terrasse 2.0';
-	title = 'titre';
+	title : string = 'Terrasse 2.0';
 }
--- terrasse-front/src/app/app.module.ts
+++ terrasse-front/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
