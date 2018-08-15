8f34326683c742cebd838330c0bfe2f80ec3130f
sylvain faubet <faubet.sylvain@gmail.com>
2018-08-16 00:54:04 +0200

    finish placement of footer

--- terrasse-front/src/app/app.module.ts
+++ terrasse-front/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,7 @@ import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
 	declarations: [AppComponent, DashboardComponent, FooterComponent],
-	imports: [BrowserAnimationsModule, BrowserModule, AppRoutingModule, MaterialModule],
+	imports: [BrowserAnimationsModule, BrowserModule, AppRoutingModule, MaterialModule, FlexLayoutModule],
 	exports: [MaterialModule],
 	providers: [],
 	bootstrap: [AppComponent],
