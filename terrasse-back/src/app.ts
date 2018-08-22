1905ea8e1526dc99cae5c1faf0a1ab4b254f7fd9
elsa faubet <elsa.faubet@outlook.fr>
2018-08-22 20:14:58 +0200

    hmr on back

--- terrasse-back/src/app.ts
+++ terrasse-back/src/app.ts
@@ -1,16 +1,17 @@
-import projects from './mocks/projects';
+import projects from "./mocks/projects";
 export default {
-    getHandler(req:any,res:any){
-        console.log(req.url);
-        switch(req.url){
-            case '/projects':
-            res.send(projects);
-            break;
-
-            default:
-            res.send("hello world !");
-        }
-        
-       
+  getHandler(req: any, res: any) {
+    console.log(req.url);
+    switch (req.url) {
+      case "/projects":
+        res.send(projects);
+        break;
+      case "/terrasses":
+        res.send(projects[0].terrasses);
+        break;
+      case "/":
+      default:
+        res.send("hello world !");
     }
-}
+  }
+};
