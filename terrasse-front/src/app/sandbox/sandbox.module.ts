fa7cb4ad2c9c6d3568df82013bebdb62cb44868c
sylvain faubet <faubet.sylvain@gmail.com>
2018-09-25 13:23:28 +0200

    minor fixes

--- terrasse-front/src/app/sandbox/sandbox.module.ts
+++ terrasse-front/src/app/sandbox/sandbox.module.ts
@@ -4,9 +4,10 @@ import { SandboxComponent } from './sandbox.component';
 import { SandboxRoutingModule } from './sandbox-routing.module';
 import { AppCommonModule } from '../app.module';
 import { ProjectsModule } from '../projects/projects.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-    imports: [SandboxRoutingModule, AppCommonModule, ProjectsModule],
+    imports: [SandboxRoutingModule, AppCommonModule, ProjectsModule, FlexLayoutModule],
     declarations: [SandboxComponent],
 })
 export class SandboxModule {}
