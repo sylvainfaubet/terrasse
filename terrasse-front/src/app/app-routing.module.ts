6e506e88bcf5ac02c9d7b29c24101059646a8c43
sylvain faubet <faubet.sylvain@gmail.com>
2018-11-30 18:47:08 +0100

    remove unuseful code

--- terrasse-front/src/app/app-routing.module.ts
+++ terrasse-front/src/app/app-routing.module.ts
@@ -6,20 +6,15 @@ import { environment } from '../environments/environment';
 export const routes: Routes = [
     // { path: 'home', component: DashboardComponent },
     { path: '', pathMatch: 'full', redirectTo: 'projects/1' },
-    // {
-    //     path: 'sandbox',
-    //     canLoad: ['canLoadSanbox'],
-    //     loadChildren: './sandbox/sandbox.module#SandboxModule',
-    // },
+    {
+        path: 'sandbox',
+        canLoad: ['canLoadSanbox'],
+        loadChildren: './sandbox/sandbox.module#SandboxModule',
+    },
     {
         path: 'projects',
         loadChildren: './projects/projects.module#ProjectsModule',
     },
-    // {
-    //     path: 'kito',
-    //     canLoad: ['canLoadKito'],
-    //     loadChildren: './kito/kito.module#KitoModule',
-    // },
 ];
 
 @NgModule({
@@ -29,10 +24,6 @@ export const routes: Routes = [
         {
             provide: 'canLoadSanbox',
             useValue: () => !environment.production,
-        },
-        {
-            provide: 'canLoadKito',
-            useValue: () => true,
         },
     ],
 })
