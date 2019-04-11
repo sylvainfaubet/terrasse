127cd50c336e793121c08a798949189ebe791d99
sylvain.faubet <faubet.sylvain@gmail.com>
2019-04-11 14:26:34 +0200

    prod !!!!

--- terrasse-front/src/app/polygon/polygon.module.ts
+++ terrasse-front/src/app/polygon/polygon.module.ts
@@ -1,9 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PolygonConfigComponent } from './polygon-config/polygon-config.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
-    imports: [CommonModule],
-    declarations: [],
+    imports: [CommonModule, FlexLayoutModule, MaterialModule],
+    declarations: [PolygonConfigComponent],
+    exports: [PolygonConfigComponent],
     providers: [],
 })
 export class PolygonModule {}
