c3dde01e79f70cfca18d0de88358b9fdc81622fd
Sylvain FAUBET <sylvain.faubet@insidegroup.fr>
2019-11-13 18:59:05 +0100

    refactor : change files structure

---
+++ terrasse-front/src/app/components/index.js
@@ -0,0 +1,9 @@
+import { HeaderComponent } from './header/header.component';
+import { AppComponent } from './app/app.component';
+import { FooterComponent } from './footer/footer.component';
+
+export * from './app/app.component';
+export * from './header/header.component';
+export * from './footer/footer.component';
+
+export const components = [HeaderComponent, FooterComponent, AppComponent];
