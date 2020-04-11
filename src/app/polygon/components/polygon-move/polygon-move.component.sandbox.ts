3898fc2b157954bdf693f110ec35002aa0b15b5e
Sylvain FAUBET <sylvain.faubet@insidegroup.fr>
2020-04-11 11:20:43 +0200

    remove angular-playground

--- src/app/polygon/components/polygon-move/polygon-move.component.sandbox.ts
+++
@@ -1,7 +0,0 @@
-import { sandboxOf } from 'angular-playground';
-import { PolygonMoveComponent } from './polygon-move.component';
-
-export default sandboxOf(PolygonMoveComponent)
-  .add('default', {
-    template: `<terrasse-polygon-move></terrasse-polygon-move>`
-  });
--- src/app/project/components/configure/configure.component.sandbox.ts
+++
@@ -1,7 +0,0 @@
-import { sandboxOf } from 'angular-playground';
-import { ConfigureComponent } from './configure.component';
-
-export default sandboxOf(ConfigureComponent)
-  .add('default', {
-    template: `<terrasse-configure></terrasse-configure>`
-  });
