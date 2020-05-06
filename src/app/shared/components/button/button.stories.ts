b939001b8261d4a6ff766e1b8db76b100191b345
Sylvain FAUBET <sylvain.faubet@insidegroup.fr>
2020-05-06 08:21:49 +0200

    add knobs on storybook

--- src/app/shared/components/button/button.stories.ts
+++ src/app/shared/components/button/button.stories.ts
@@ -2,9 +2,11 @@ import { storiesOf } from '@storybook/angular';
 import { ButtonComponent } from "./button.component";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from '@angular/material/icon';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 
 storiesOf('terrasse-button', module)
-  .add("texte", () => ({
+  .addDecorator(withKnobs)
+  .add("knobs", () => ({
     moduleMetadata: {
       declarations: [ButtonComponent],
       imports: [
@@ -13,22 +15,10 @@ storiesOf('terrasse-button', module)
       ]
     },
     props: {
-      text: "tester"
+      text: text("text", "tester"),
+      disabled: boolean("disabled", false),
+      icon: text('icon', "")
     },
-    template: '<terrasse-button>{{text}}</terrasse-button>'
-  }))
-  .add("icone", () => ({
-    moduleMetadata: {
-      declarations: [ButtonComponent],
-      imports: [
-        MatButtonModule,
-        MatIconModule,
-      ]
-    },
-    props: {
-      text: "tester",
-      icon: "add"
-    },
-    template: '<terrasse-button [icon]="icon"></terrasse-button>'
+    template: '<terrasse-button [icon]="icon" [disabled]="disabled">{{text}}</terrasse-button>'
   }))
 
